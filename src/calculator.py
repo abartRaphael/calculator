a73a804fa6f7c79f468d305e231b1f6d015d1aaf
Matthieu Boulanger <matthieu.boulanger.etu@univ-lemans.fr>
2026-01-07 10:59:42 +0100

    implémentation de la multiplication

--- src/calculator.py
+++ src/calculator.py
@@ -2,3 +2,6 @@ class Calculator :
     # return the sum between 2 operands
     def mysum(self, first_operand, second_operand):
         return first_operand + second_operand
+
+    def myprod(self, first_operand, second_operand):
+        return first_operand * second_operand
