96b126fce7e598647fb46df56f0be80eaf5203e2
Matthieu Boulanger <matthieu.boulanger.etu@univ-lemans.fr>
2026-01-07 10:58:04 +0100

    ajout du test de la multiplication

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -8,6 +8,9 @@ class TestCalculator(TestCase):
     def test_sum(self):
         self.assertEqual(self.calc.mysum(1, 2), 3)
 
+    def test_product(self):
+        self.assertEqual(self.calc.myprod(2, 4), 8)
+
     
     
 if __name__ == '__main__':
